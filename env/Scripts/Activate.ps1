4c5634b95559d234975aab51b7d74880adac6bc3
bryancano <bcanop@miumg.edu.gt>
2021-10-15 17:28:49 -0600

    Categorias, productos y clientes funcionando, video 56

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Projects\hades\env"
+$env:VIRTUAL_ENV="C:\Users\bryan\Documents\hades_ProyectoFinal_DW\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
